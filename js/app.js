81ab49520a23f2aacc789b2dcc67b07f9b24fff3
alexito2 <85403880+alexito2@users.noreply.github.com>
2022-04-13 17:51:05 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js')
-        .then(function() {
-            console.log('ServiceWorker registered!');
-        })
-        .catch(function(err) {
-            console.log('ServiceWorker failed :(', err);
-        });
-    });
-  }
+//confirmar si soporta sw
+if ( navigator.serviceWorker ){
+  //alert("Soporta el SW");
+  navigator.serviceWorker.register("/sw.js");
+}
+else{
+  alert(":( no soporta el SW");
+}
+
+
+
+
+
+
+//if ('serviceWorker' in navigator) {
+  //  window.addEventListener('load', function() {
+    //  navigator.serviceWorker.register('/sw.js')
+      //  .then(function() {
+        //    console.log('ServiceWorker registered!');
+        //})
+        //.catch(function(err) {
+          //  console.log('ServiceWorker failed :(', err);
+       // });
+   // });
+  //}
